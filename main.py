a76d9210f0b539f49c247eda808cf22f1f929ad8
KirillMos1 <kirillkasparyants@yandex.com>
2025-01-18 20:39:36 +0300

    Create 0.0.1_alpha version

---
+++ main.py
@@ -0,0 +1,12 @@
+print("KirillkasCode v.0.0.1_alpha for x64")
+while True:
+	com = input(">> ")
+	com_list = com.split(" ")
+	if com_list[0] = "print":
+		com_list.remove(0)
+		returned = ""
+		for arg in com_list:
+			returned += agr + " "
+		print(returned)
+	else:
+		print("I can`t do this!")
