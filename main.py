454f03b00904a383e7fc291db6b3e45607972847
KirillMos1 <kirillkasparyants@yandex.com>
2025-01-22 17:22:51 +0300

    Add repeats and importing modules. End of 0.1.3_alpha

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from lib.standart.delay import delay
+
 vars_list = {}
+lib_imported = []
+libs = {"delay" : ["delay"]}
 
 def compiler(path):
     try:
@@ -39,8 +43,6 @@ def compiler(path):
         printer(["Error. Prosess: main/compiler: Description: file not found!"])
     except Exception as e:
         printer([f"Error. Prosess: main/compiler. {e}"])
-            
-
 
 def printer(args: list):
     try:
@@ -75,7 +77,7 @@ def delvar(name: str):
         printer(["Error. Prosess: main/delvar"])
 
 def info():
-    printer(["Commands of KKC: \ncompiler <path> - run compile on file <path>\ninfo - this message \nprint <arg1> <arg2> <arg_n> - print a args \nsetvar <name> <val>- create a var <name> \ndelvar <name> - delete var <name> \nexit - exit from KKC \nmath <exp> - complete expression <exp> \nmath_vars <var1> <var2> <symbol> - math operation <symbol> with <var1> and <var2> \nequals_vars <var1> <var2> - equaling of vars (true / false) \ncompile <path> - compile and run file <path>"])
+    printer(["Commands of KKC: \ncompiler <path> - run compile on file <path>\ninfo - this message \nprint <arg1> <arg2> <arg_n> - print a args \nsetvar <name> <val>- create a var <name> \ndelvar <name> - delete var <name> \nexit - exit from KKC \nmath <exp> - complete expression <exp> \nmath_vars <var1> <var2> <symbol> - math operation <symbol> with <var1> and <var2> \nequals_vars <var1> <var2> - equaling of vars (true / false) \ncompile <path> - compile and run file <path> \nrepeat <repeats> <commands> - run <commands> <repeats> times \nimport <module> - import module <module> in KKC"])
 
 def math_func(exp):
     try:
@@ -104,6 +106,51 @@ def equals_to_vars(var1, var2):
             printer(["False"])
     except:
         printer(["Error. Prosess: main/equals_vars"])
+
+def repeats(reps: int, coms: list):
+    try:
+        for i in range(reps):
+            for com in coms:
+                com_list = com.split("|")
+                func = com_list[0]
+                args = com_list[1].split("_")
+                if func == "print":
+                    printer(args)
+                elif func == "setvar":
+                    setvar(args[0], args[1])
+                elif func == "delvar":
+                    delvar(args[0])
+                elif func == "info":
+                    info()
+                elif func == "exit":
+                    raise SystemExit(0)
+                elif func == "math":
+                    math_func(args[0])
+                elif func == "math_vars":
+                    math_vars(args[0], args[1], args[2])
+                elif func == "input":
+                    if len(args) == 2:
+                        inputing(args[0], args[1])
+                    else:
+                        inputing(args[0])
+                elif func == "equals_vars":
+                    equals_to_vars(args[0], args[1])
+                elif func == "":
+                    continue
+                else:
+                    printer(["I can`t do this!"])
+    except:
+        printer(["Error. Prosess: main/repeats"])
+
+def importing(lib):
+    try:
+        global lib_imported, libs
+        if lib in libs.keys():
+            lib_imported.append(lib)
+        else:
+            printer(["Error. Prosess: main/import. Description: module not found"])
+    except:
+        printer(["Error. Prosess: main/import. Description: module not found"])
 
 print("KirillkasCode v.0.1.2_alpha for x64")
 while True:
@@ -148,7 +195,19 @@ while True:
         var1 = input("var1>> ")
         var2 = input("var2>> ")
         equals_to_vars(var1, var2)
+    elif com == "repeat":
+        repets = int(input("repeats (num)>> "))
+        args = []
+        arg = "n"
+        while arg != "":
+            arg = input("com (.kkc format)>> ")
+            args.append(arg)
+        args.remove("")
+        repeats(repets, args)
     elif com == "":
         continue
+    elif com == "import":
+        lib = input("module>>")
+        importing(lib)
     else:
         printer(["I can`t do this!"])
